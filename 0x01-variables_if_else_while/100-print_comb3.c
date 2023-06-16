44697c43f7bdb730d23d13c9469548acb2688a75
itswanja <wanjacharity92@gmail.com>
2023-06-16 12:07:35 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,19 +7,17 @@ int main(void)
 {
 	int n, m;
 
-	for (n = 48; n <= 56; n++)
+	for (n = 48; n < 56; n++)
 	{
-		for (m = 49; m <= 57; m++)
+		for (m = 49; m <= 56; m++)
 		{
-			if (m > n)
+			if (m != n)
 			{
 				putchar(m);
 				putchar(n);
-				if (n != 56 && m != 57)
-				{
+				if (n == 56 && m == 57)
 					putchar(',');
 					putchar(' ');
-				}
 			}
 		}
 	}
