d81933729a361cf887703e6085772fb037eb0a2f
itswanja <wanjacharity92@gmail.com>
2023-06-16 10:54:36 -0700

    gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -o quote 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - A program that prints a line to the standard error
  * Return: 1 (Success)
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
