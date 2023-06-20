608a118fd7c6c76b7713c64a38141c9562fe2871
itswanja <wanjacharity92@gmail.com>
2023-06-20 02:00:39 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - a function that prints the alphabet, in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <=122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
