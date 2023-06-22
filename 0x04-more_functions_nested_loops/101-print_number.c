21101ea7c5ba9ea85ca790b97b51397a82ae609f
itswanja <wanjacharity92@gmail.com>
2023-06-22 09:03:59 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_number - Prints an integer
+ * @n: The integer to be printed
+ * Return: Nothing
+ */
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		i = n;
+		_putchar('-');
+	}
+	i /= 10;
+	if (i != 0)
+		print_number(i);
+	_putchar((unsigned int) n % 10 + '0');
+}
