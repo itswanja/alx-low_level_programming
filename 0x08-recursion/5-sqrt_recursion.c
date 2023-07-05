bd5777214c78d41e992908f5c8b667d2765111b1
itswanja <wanjacharity92@gmail.com>
2023-07-05 03:41:31 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,7 +12,7 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (_sqrt_recursion(n, 0));
+	return (actual_sqrt_recursion(n, 0));
 }
 /**
  * actual_sqrt_recursion - calculates natural square root
@@ -27,3 +27,4 @@ int actual_sqrt_recursion(int n, int i)
 	if (i * i == n)
 		return (1);
 	return (actual_sqrt_recursion(n, i + 1));
+}
