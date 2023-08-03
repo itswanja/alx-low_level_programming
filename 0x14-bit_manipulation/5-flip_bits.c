58983ed38da88835f6facb9876597e514d95ae85
itswanja <wanjacharity92@gmail.com>
2023-08-03 06:46:52 -0700

    4-clear_bit.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits
+ * you would need to flip to get from one number to another
+ * @n: 
+ * @m:
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
